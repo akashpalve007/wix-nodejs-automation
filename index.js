54d4ab784c2d90982bdec941610e8b2c7c382085
akashpalve007 <akash.palve.techinvento@gmail.com>
2024-08-12 10:20:31 +0530

    changed env creds

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
@@ -5,7 +6,7 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-const VERIFY_TOKEN = "whatsapp_bot_12345";
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
 // Webhook verification
 app.get("/webhook", (req, res) => {
@@ -14,8 +15,10 @@ app.get("/webhook", (req, res) => {
   const challenge = req.query["hub.challenge"];
 
   if (mode && token === VERIFY_TOKEN) {
+    console.log("Webhook verified successfully");
     res.status(200).send(challenge);
   } else {
+    console.log("Webhook verification failed");
     res.sendStatus(403);
   }
 });
@@ -33,37 +36,39 @@ app.post("/webhook", async (req, res) => {
           const from = messageData.from; // The WhatsApp ID of the user who sent the message
           const msg_body = messageData.text.body; // The message text
 
-          console.log("Message received from:", from);
-          console.log("Message text:", msg_body);
+          console.log(`Message received from ${from}: ${msg_body}`);
 
-          // Respond with a message
-          await sendWhatsAppMessage(
-            from,
-            "Thanks for your message! How can I help you?"
-          );
+          // Send an automated response
+          const responseMessage =
+            "Thanks for your message! How can I help you?";
+          await sendWhatsAppMessage(from, responseMessage);
+
+          console.log(`Automated message sent to ${from}: ${responseMessage}`);
         }
       });
     });
     res.sendStatus(200);
   } else {
+    console.log("Webhook received unsupported event type");
     res.sendStatus(404);
   }
 });
 
 async function sendWhatsAppMessage(to, message) {
-  const url = `https://graph.facebook.com/v20.0/356986240839037/messages`;
+  const url = `https://graph.facebook.com/v19.0/${process.env.PHONE_NUMBER_ID}/messages`;
   const data = {
     messaging_product: "whatsapp",
     to: to,
     text: { body: message },
   };
   try {
-    await axios.post(url, data, {
+    const response = await axios.post(url, data, {
       headers: {
-        Authorization: `Bearer EAAXSzbP7mioBO12HoKqxzWeHoaI1XAgHJwC5btVZAcE0Nvk3ioDl6KLXnbHx2rG8h1rd5LRZBiLdqFOmZBRVGwh0L8wZBuO2e84HleHbajyU0xQZCbBFJbpAh83LtOWgAsNeZB9oogu0WfmxAtXjkKq6p9RhwWdIk9z2gdjWZCAtfAuqZCEZCLt0Q8cKNKJrZCeruncQZDZD`,
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
         "Content-Type": "application/json",
       },
     });
+    console.log(`Message sent successfully: ${response.data}`);
   } catch (error) {
     console.error(
       "Error sending message:",
